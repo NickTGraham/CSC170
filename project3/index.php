57b250c7e69645aa7e048c31c1cb99c3f04ca732
Nick Graham <nickdud341@gmail.com>
2014-11-22 16:24:17 -0500

    Fixed image issue on index page

--- project3/index.php
+++ project3/index.php
@@ -22,29 +22,29 @@
 
   <article>
     <section class="overview">
-      <img src="images/aaron.jpg" alt="Aaron">
       <h2>Aaron Swartz</h2>
+      <img src="images/aaron.jpg" alt="Aaron">
       <p>Aaron Hillel Swartz (November 8, 1986 – January 11, 2013) was an American computer programmer, writer, political organizer and Internet Hacktivist. Swartz was involved in the development of the web feed format RSS and the Markdown publishing format, the organization Creative Commons, the website framework web.py and the social news site, Reddit, in which he became a partner after its merger with his company, Infogami. Swartz's work also focused on sociology, civic awareness and activism.</p>
       <a href="aaron.php">Read more about Aaron Swartz</a>
     </section>
 
     <section class="overview">
-      <img src="images/radpearl.jpg" alt="Radia">
       <h2>Radia Pearlman</h2>
+      <img src="images/radpearl.jpg" alt="Radia">
       <p>Radia Joy Perlman (born 1951) is a software designer and network engineer. She is most famous for her invention of the spanning-tree protocol (STP), which is fundamental to the operation of network bridges, while working for Digital Equipment Corporation. She also made large contributions to many other areas of network design and standardization, such as link-state protocols, including TRILL, which she invented to correct some of the shortcomings of spanning-trees.</p>
       <a href="radia.php">Read more about Radia Pearlman</a>
     </section>
 
     <section class="overview">
-      <img src="images/mugshot.jpg" alt="Marissa">
       <h2>Marissa Mayer</h2>
+      <img src="images/mugshot.jpg" alt="Marissa">
       <p>Marissa Mayer, born on May 30, 1975, in Wausau, Wisconsin, was appointed CEO of Yahoo in 2012. Prior to joining Yahoo, she spent 13 years at Google, where her work in product development largely contributed to the site's unique look and feel. At the time of Mayer's Yahoo appointment, she was one of <em>only 20</em> female CEOs in charge of a Fortune 500 company.</p>
       <a href="marissa.php">Read more about Marissa Mayer</a>
     </section>
 
     <section class="overview">
-      <img src="images/Alan-Turing.jpg" alt="Alan">
       <h2>Alan Turing</h2>
+      <img src="images/turing.jpg" alt="Alan">
       <p>Alan Mathison Turing was a British mathematician, logician, cryptanalyst, philosopher, computer scientist, mathematical biologist, and marathon and ultra distance runner. He was highly influential in the developmemt of computer science, providing a formalisation of the concepts of "algorithms" and "computations" with the Turing machine, which can be considered a model of a general purpose machine. Turing is widely considered to be the father of theoretical computer science and artificial intelligence.</p>
       <a href="alan.php">Read more about Alan Turing</a>
     </section>
